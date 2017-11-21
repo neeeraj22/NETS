889bd423d50e31c493216fcc37725499a16e9b67
Neeraj Gupta <neerajgupta@Neerajs-MacBook-Pro.local>
2017-11-21 19:51:34 +0100

    added new code base

---
+++ src/Test.java
@@ -0,0 +1,18 @@
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
+public class Test {
+
+
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		BasicConfigurator.configure();
+		final Logger logger = Logger.getLogger(Test.class);
+		logger.info("Hello Neeraj Gupta");
+
+		//logger.debug("Hello World!");
+
+	}
+
+}
